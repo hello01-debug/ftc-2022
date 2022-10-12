5cbc72c6589556aa21a30a3ca94219756bdaba2e
traczpasruchu <37715170+traczpasruchu@users.noreply.github.com>
2022-10-12 18:36:14 -0400

    Update NewManualOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/NewManualOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/NewManualOp.java
@@ -11,5 +11,26 @@ public class NewManualOp extends OpMode {
 
     public void init() {
         robot = new Robot(hardwareMap, telemetry);
+        robot.runUsingEncoders();
+        controller1 = new Controller(gamepad1);
+        controller2 = new Controller(gamepad2);
+    }
+
+    public void loop() {
+        controller1.update();
+        controller2.update();
+
+        double y = controller1.left_stick_y;
+        double x = controller1.left_stick_x;
+        double rot = controller1.right_trigger - controller2.right_trigger;
+
+        final double leftFront = y + x + rot;
+        final double leftRear = y - x + rot;
+        final double rightFront = y - x - rot;
+        final double rightRear = y + x - rot;
+
+        robot.setMotors(leftFront, leftRear, rightFront, rightRear);
+
+        // need to implement slide in robot class before continuing
     }
 }
