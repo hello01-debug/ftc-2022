601959e9ef5553501e493cb56629fd20ad8116f8
Your Name <you@example.com>
2022-12-05 17:56:56 -0500

    Why did we have two gonkas? And what the heck is a gonka, anyways?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/gonkaCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/gonkaCamera.java
@@ -16,6 +16,7 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.firstinspires.ftc.teamcode.drive.opmode.vision.gonkaPipeline;
 
 @Autonomous
 public class gonkaCamera extends OpMode {
@@ -45,57 +46,4 @@ public class gonkaCamera extends OpMode {
     public void loop() {
 
     }
-
-    public class gonkaPipeline extends OpenCvPipeline {
-        Mat YcBcr = new Mat();
-        Mat output = new Mat();
-        Mat[] verticalMat = new Mat[20];
-        Rect[] verticalSlices = new Rect[20];
-        Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
-        Scalar[] verticalAvg = new Scalar[20];
-        Scalar lower = new Scalar(0.0, 138.8, 51.0);
-        Scalar upper = new Scalar(255.0, 178.5, 94.9);
-        Mat mask = new Mat();
-        int left, right;
-
-        public Mat processFrame(Mat input) {
-            left = 0;
-            right = 0;
-
-            Imgproc.cvtColor(input, YcBcr, Imgproc.COLOR_RGB2YCrCb);
-
-            Core.inRange(YcBcr, lower, upper, mask);
-
-            YcBcr.copyTo(output);
-
-            for (int currentRect = 0; currentRect < 20; currentRect++) {
-                verticalSlices[currentRect] = new Rect((currentRect * 64), 1, 64, 719);
-                verticalMat[currentRect] = mask.submat(verticalSlices[currentRect]);
-                verticalAvg[currentRect] = Core.mean(verticalMat[currentRect]);
-                if (verticalAvg[currentRect].val[0] > 128) {
-                    Imgproc.rectangle(output, verticalSlices[currentRect], new Scalar(0.0, 255.0, 0.0), 2);
-                    if (currentRect < 10) {
-                        left++;
-                    } else {
-                        right++;
-                    }
-                } else {
-                    Imgproc.rectangle(output, verticalSlices[currentRect], rectColor, 2);
-                }
-                if (left > right) {
-                    Imgproc.putText(output,"Move Left", new Point(360, 360), Imgproc.FONT_HERSHEY_SIMPLEX, 5, new Scalar(0.0, 0.0, 255.0), 5);
-                } else if (right > left) {
-                    Imgproc.putText(output,"Move Right", new Point(360, 360), Imgproc.FONT_HERSHEY_SIMPLEX, 5, new Scalar(0.0, 0.0, 255.0), 5);
-                }
-            }
-
-            // Basically i'll put up a mask that only lets yellow through, then it will check to see which square has the most visible (non-masked) pixels to see where the pole would be
-
-            //Imgproc.putText(output, "test text", new Point(25, 100), Imgproc.FONT_HERSHEY_COMPLEX, 2.0, new Scalar(255.0, 0.0, 0.0));
-
-            return output;
-
-        }
-    }
-
 }
